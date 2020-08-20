eadf2d7f1ce2a39fa2aa1a02cf5a43be16939aa8
Damian Bemben <bembendamian@yahoo.co.uk>
2020-08-20 21:38:38 +0100

    Some updates on portfolio  site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-                  Portfolio Website Initial Commit
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render() {
+        return (
+            <div className="App-main">
+                <canvas />
+            </div>
+            )
+    }
 }
 
 export default App;
