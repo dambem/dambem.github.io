39d0ab67327b5fac6b1a5e361f876dc55106eb02
Damian <bembendamian@yahoo.co.uk>
2020-08-23 23:33:34 +0100

    Updates

---
+++ precache-manifest.3e5826818f45113446cd847dbb753f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3ceee89b7509c192ef5852f8712d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "235c889e8750ac81bbc8",
+    "url": "/static/css/main.b6bc1957.chunk.css"
+  },
+  {
+    "revision": "45279310d16af545423f",
+    "url": "/static/js/2.19a67341.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19a67341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235c889e8750ac81bbc8",
+    "url": "/static/js/main.3287c38e.chunk.js"
+  },
+  {
+    "revision": "f1e11b0b61793e4aa1b2",
+    "url": "/static/js/runtime-main.304afa60.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
